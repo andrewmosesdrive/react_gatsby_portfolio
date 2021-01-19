e816739c215816c8a2459f2cfb486f9bbf212484
Andrew Moses <andrewmosesdrive@gmail.com>
2021-01-18 18:19:20 -0700

    deleted unnecessary files

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,10 +8,6 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/src/pages/404.js"))),
   "component---src-pages-creative-2-index-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/src/pages/creative-2/index.js"))),
-  "component---src-pages-creative-video-1-index-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/src/pages/creative-video-1/index.js"))),
-  "component---src-pages-creative-video-2-index-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/src/pages/creative-video-2/index.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/src/pages/index.js"))),
-  "component---src-pages-personal-index-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/src/pages/personal/index.js"))),
-  "component---src-pages-personal-video-index-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/src/pages/personal-video/index.js")))
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/src/pages/index.js")))
 }
 
