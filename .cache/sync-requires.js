df416b118e6d3784fa499abcd869ecc81da442ca
Andrew Moses <andrewmosesdrive@gmail.com>
2021-01-18 18:27:48 -0700

    deploy

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/src/pages/404.js"))),
-  "component---src-pages-creative-2-index-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/src/pages/creative-2/index.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/andrewmoses/Desktop/repos/homework_repos/react_gatsby_portfolio/src/pages/index.js")))
 }
 
